e9e882c6ab55267c7e76d5c4f6be6d19d823e90b
HQ\abe <abinesh.e@kudelskisecurity.com>
2024-11-05 12:23:51 +0530

    all done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useLayoutEffect } from 'react';
 
 function App() {
-  //const [count, setCount] = useState(0);
-  
+  const [count, setCount] = useState(0);
+  const [state, setState] = useState(0);
+  console.log('*** Component Re-rendered ' + count, state);
+  useEffect(() => {
+    console.log('Use Effect is runing *** ' + count, state);
+    setState((p) => p + 1);
+  }, [count]);
+  useLayoutEffect(() => {
+    console.log('************* something got painted on to the screen');
+  });
+
   return (
     <>
       <div>
@@ -17,9 +29,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        {/* <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
-        </button> */}
+        </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
